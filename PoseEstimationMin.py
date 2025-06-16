e1a7fe0b371159cf96a02459d5e451aa3f8731aa
Peehu_agrawal <166029190+Peehuagrawal@users.noreply.github.com>
2025-06-16 19:50:27 +0530

    initial

--- PoseEstimationMin.py
+++ PoseEstimationMin.py
@@ -1,2 +1,32 @@
 import cv2
-import mediapipe
+import mediapipe as mp
+import time
+
+
+cap = cv2.VideoCapture("PoseVideos/1.mp4")
+pTime = 0
+
+while True:
+    success, img = cap.read()
+
+    if not success or img is None:
+        print("Video has ended. Press any key to exit.")
+        cv2.waitKey(0)  # Wait indefinitely for a key press
+        break
+
+    cTime = time.time()
+    time_diff = cTime - pTime
+    fps = 1 / time_diff if time_diff > 0 else 0
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (70, 50),
+                cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+
+    # Break on 'q' key
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
